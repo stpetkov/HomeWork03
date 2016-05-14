a443b5a5babf46f980224b160fbb42275a7c3e9f
Stanimir <stpetkov4@gmail.com>
2016-05-14 11:34:35 +0300

    Task 02 of the Conditions HomeWork.

---
+++ src/Task02.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Task02 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in,"UTF-8");
+		int number1=sc.nextInt();
+		int number2=sc.nextInt();
+		if(number1>number2){
+			System.out.println(number1+" is bigger");
+		}
+		else if(number1==number2){
+			System.out.println("Equal");
+		}
+		else {
+			System.out.println(number2+" is bigger");
+		}
+
+	}
+
+}
