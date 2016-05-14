89204849b96c1f627691b407415d675dfa813133
Stanimir <stpetkov4@gmail.com>
2016-05-14 12:21:24 +0300

    Task 03 from Conditions HomeWork.

--- src/Task03.java
+++ src/Task03.java
@@ -13,7 +13,7 @@ public class Task03 {
 		else if(number2>number1 && number2>number3){
 			System.out.println(number2+" is the biggest");
 		}
-		else if(number3>number1 && number3>number1){
+		else if(number3>number1 && number3>number2){
 			System.out.println(number3+" is the biggest");
 		}
 		else {
