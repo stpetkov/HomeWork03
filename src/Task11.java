206c4d6a8170a2fb862a10aba64acc6d3b5d49b1
Stanimir <stpetkov4@gmail.com>
2016-05-16 10:53:03 +0300

    Task 01 from Loops HomeWork.

---
+++ src/Task11.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Task11 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in, "UTF-8");
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++){
+			System.out.print(" "+i);
+		}
+		sc.close();
+	}
+
+}
